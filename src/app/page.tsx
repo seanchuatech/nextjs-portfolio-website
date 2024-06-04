c2a6e531d2a21c8d60e0ba16512e07815b8e9b80
Sean Leonard Chua <seanchuatech.com>
2024-06-04 11:14:59 +0800

    website update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Skills from "./ui/home/skills";
 import Projects from "./components/Projects";
 import Hero from "./components/Hero";
 import Contact from "./components/Contact";
-import About from "./components/about";
+import About from "./components/About";
 
 export default function Home() {
   return (
